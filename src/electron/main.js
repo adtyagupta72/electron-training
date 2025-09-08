025a2223e2b81dede2d8a86812ac2f8033ffb89b
Aditya Gupta <adtyagupta72@gmail.com>
2025-09-08 12:46:40 +0530

    First commit

---
+++ src/electron/main.js
@@ -0,0 +1,8 @@
+import {app, BrowserWindow} from 'electron';
+import path from 'path';
+
+app.on('ready', ()=> 
+{
+    const mainWidow = new BrowserWindow({})
+    mainWidow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'))
+})
